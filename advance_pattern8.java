34db27afd3857afc66cbf578a995df332363ba69
Sarthak543 <sarthakjaiswal543@gmail.com>
2023-02-12 19:34:42 +0530

    pwskills

--- advance_pattern8.java
+++ advance_pattern8.java
@@ -2,7 +2,7 @@ public class advance_pattern8 {
     public static void main(String[] args) {
         int n = 11;
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
+            for (int j = 0; j < n; j++) { //P
                 if ((i == 0 && j <= (n - 1) / 2) || j == 0 || (i == (n - 1) / 2 && j <= (n - 1) / 2)
                         || (j == (n - 1) / 2 && i <= (n - 1) / 2)) {
                     System.out.print("*");
@@ -10,16 +10,33 @@ public class advance_pattern8 {
                     System.out.print(" ");
                 }
             }
+            
             System.out.print("   ");
-            for (int j = 0; j < n; j++) {
+            for (int j = 0; j < n; j++) { //W
                 if (j==0 || j==n-1 || (i+j==n-1 && j<=(n-1)/2) || (i==j && j>=(n-1)/2)) {
                     System.out.print("*");
                 } else {
                     System.out.print(" ");
                 }
             }
-
-            //
+            
+            System.out.print("                          ");
+            for (int j = 0; j < n; j++) { //S
+                if (((i==0 || i==n-1 || i==(n-1)/2) && j<=(n-1)/2)  || (j==0 &&i<=(n-1)/2) || (j==(n-1)/2 &&i>=(n-1)/2)) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+           
+            System.out.print("      ");
+            for (int j = 0; j < n; j++) { //K
+                if (j==0 || (i+j==(n-1)/2) || (i-j==(n-1)/2)) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }              
 
             System.out.println();
         }
