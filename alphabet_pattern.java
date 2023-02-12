c6889ff1fffcea77a5703491f5c03b957a84365c
Sarthak543 <sarthakjaiswal543@gmail.com>
2023-02-12 18:20:54 +0530

    A-H alphabet pattern

---
+++ alphabet_pattern.java
@@ -0,0 +1,79 @@
+public class alphabet_pattern {
+    public static void main(String[] args) {
+        int n=15;
+        for (int i = 0; i < n; i++) { //A
+            for (int j = 0; j < n; j++) {
+                if ((i==0 && (j!=0 && j!=(n-1))) || (j==0&& i!=0 ) || (j==n-1 && i!=n-1) || i==(n-1)/2) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            
+            System.out.print("    ");
+            for (int j = 0; j < n; j++) { //B
+                if (i==0 || j==0 || j==n-1 || i==(n-1)/2 || i==n-1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+
+            System.out.print("    ");
+            for (int j = 0; j < n; j++) {//C
+                if (i==0 || i==n-1 || j==0) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+
+            System.out.print("    ");
+            for (int j = 0; j < n; j++) {//D
+                if (i==0 || j==0 || i==n-1 || j==n-1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+
+            System.out.print("    ");
+            for (int j = 0; j < n; j++) {//E
+                if (j==0 || i==0 || i==(n-1)/2 || i==n-1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+
+            System.out.print("    ");
+            for (int j = 0; j < n; j++) {//F
+                if (j==0 || i==0 || i==(n-1)/2) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+
+            System.out.print("    ");
+            for (int j = 0; j < n; j++) {//G
+                if ((i==0 && j<=(n-1)/2) || j==0 || (i==n-1 && j<=(n-1)/2) || (i>=(n-1)/2 && j==(n-1)/2) || (i==(n-1)/2 && j>=(n-1)/2) || (i>=(n-1)/2 && j==n-1 )) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+
+
+            System.out.print("    ");
+            for (int j = 0; j < n; j++) {//H
+                if (i==(n-1)/2 || j==0 || j==n-1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
