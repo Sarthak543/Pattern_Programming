6e1f152cd969ee6f28f319024010ac5c02b6f8c2
Sarthak543 <sarthakjaiswal543@gmail.com>
2023-02-12 16:49:56 +0530

    advance pattern

--- advance_pattern2.java
+++ advance_pattern2.java
@@ -1,21 +1,16 @@
-import java.util.Scanner;
 public class advance_pattern2 {
     public static void main(String[] args) {
-        int n;
-       Scanner sc=new Scanner(System.in);  
-       System.out.println("entere the range");
-       n=sc.nextInt();
-       for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            if(i==j ||( i+j==n-1)){
-              System.out.println("*");
+           int n=11;
+           for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i==j || i+j==n-1){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
             }
-            else{
-                System.out.print(" ");
-            }
-        }
-        System.out.println(); 
-       }
-       sc.close();
+            System.out.println();
+           }
     }
 }
