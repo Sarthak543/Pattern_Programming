84902b46fbbcd78e0757c3a192c66a5357d55cf4
Sarthak543 <sarthakjaiswal543@gmail.com>
2023-02-12 17:55:00 +0530

    advance pattern

--- advance_patttern6.java
+++ advance_patttern6.java
@@ -1,32 +1,40 @@
 import java.util.Scanner;
+
 public class advance_patttern6 {
     public static void main(String[] args) {
         int n;
-       Scanner sc=new Scanner(System.in);  
-       System.out.println("enter the range");
-       n=sc.nextInt();
-       for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            if(i==j || i+j==n-1 || i==0 || j==0 || i==n-1 || j==n-1 || i+j==(n-1)/2 || i-j==(n-1)/2 || j-i==(n-1)/2 || i+j==((n-1)+(n-1)/2)){
-              System.out.print("*");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the range");
+        n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == j || i + j == n - 1 || i == 0 || j == 0 || i == n - 1 || j == n - 1 || i + j == (n - 1) / 2
+                        || i - j == (n - 1) / 2 || j - i == (n - 1) / 2 || i + j == ((n - 1) + (n - 1) / 2)) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
             }
-            else{
-                System.out.print(" ");
+            System.out.print(" ");
+            for (int j = 0; j < n; j++) {
+                if (i + j == (n - 1) / 2 || i - j == (n - 1) / 2 || j - i == (n - 1) / 2
+                        || i + j == (n - 1) + (n - 1) / 2) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
             }
-        }
-         System.out.print(" ");
-        for(int j=0;j<n;j++){
-            if(i+j==(n-1)/2 || i-j==(n-1)/2 || j-i==(n-1)/2 ||i+j==(n-1)+(n-1)/2){
-              System.out.print("*");
-            }
-            else{
-                System.out.print(" ");
-            }
-        }
 
-
-        System.out.println(); 
-       }
-       sc.close();
-}
+            System.out.print("  ");
+            for (int j = 0; j < n; j++) {
+                if (i + j <= (n - 1) / 2 ||  i- j >= (n - 1) / 2 || j-i>=(n-1)/2|| i+j>=(n-1)+(n-1)/2) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
 }
